5007118ddbd44f2efb23b7b1be44f530ba54274d
cynber <26402139+cynber@users.noreply.github.com>
2025-01-31 12:34:06 -0800

    test fix again

--- docs/.vitepress/theme/data/posts.data.js
+++ docs/.vitepress/theme/data/posts.data.js
@@ -2,24 +2,28 @@ import { createContentLoader } from 'vitepress';
 
 const blogPath = 'blog';
 
-console.log('process.env.VITEPRESS_BASE:', process.env.VITEPRESS_BASE);
-
 // const data = createContentLoader('blog/**/*.md', {
 const data = createContentLoader(`${blogPath}/**/*.md`, {
     transform(rawData) {
-        console.log('Raw Data:', rawData);
         return rawData
-            .map(post => ({
-                ...post,
-                frontmatter: {
-                    ...post.frontmatter,
-                    banner: process.env.VITEPRESS_BASE 
-                        ? `${process.env.VITEPRESS_BASE}${post.frontmatter.banner.replace(/^\//, '')}`
-                        : post.frontmatter.banner
-                }
-            }))
+            .map(post => {
+                const base = process.env.VITEPRESS_BASE || '';
+                return {
+                    ...post,
+                    frontmatter: {
+                        ...post.frontmatter,
+                        banner: base
+                            ? `${base}${post.frontmatter.banner.replace(/^\//, '')}`
+                            : post.frontmatter.banner
+                    },
+                    url: base
+                        ? `${base}${post.url.replace(/^\//, '')}`
+                        : post.url
+                };
+            })
             .sort((a, b) => new Date(b.frontmatter.date) - new Date(a.frontmatter.date));
     }
 });
 
+console.log('data:', data);
 export default data;
